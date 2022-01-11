d19b81519a2efe3fb0eaf99c85d0350a1abb2b75
Lucassoares10 <ls6359575@gmail.com>
2022-01-11 19:57:28 -0300

    JPA repository, injeção de depencias e data seeding

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
 
-@RestController
-@RequestMapping(value = "/users")
+@RestController  //indicar que é o rest é controlado por um controller
+@RequestMapping(value = "/users") //caminho do recurso
 public class UserResource {
 
 	@GetMapping 
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<User> findAll(){   //Response tipo especifico do spring para retornar resposta de requisiçoes web
 		User u = new User(1L,"Maria", "maria@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
